167ff9cc88a5cef12c9273771e719d8267cf34d0
Micah Bello <micahbello16@gmail.com>
2021-04-02 20:14:51 +0100

    Added support class library and basic quiz component and supporting models.

---
+++ CourseCreator.Library/Models/SimpleQuizModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCreator.Library.Models
+{
+    public class SimpleQuizModel
+    {
+        public int Id { get; set; }
+        public int SectionId { get; set; }
+        public string Question { get; set; }
+        public bool IsOpinionQuestion { get; set; }
+        public List<SimpleQuizOptionModel> Options { get; set; }
+    }
+}
---
+++ CourseCreator.Library/Models/SimpleQuizOptionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseCreator.Library.Models
+{
+    public class SimpleQuizOptionModel
+    {
+        public string Text { get; set; }
+        public bool IsAnswer { get; set; }
+    }
+}
