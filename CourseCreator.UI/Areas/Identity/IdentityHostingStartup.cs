5e80e1a2001fcb54d055f6ac1c2a96c40a1c6672
Micah Bello <micahbello16@gmail.com>
2021-05-31 14:06:10 +0100

    Added Quick Logins for Student and Instructor accounts

---
+++ CourseCreator.UI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using CourseCreator.UI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CourseCreator.UI.Areas.Identity.IdentityHostingStartup))]
+namespace CourseCreator.UI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
